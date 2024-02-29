77af96abe8d1ec8e9b15a1d733183e642182ed2e
Ernesto Guerrero <ernestojair2020@gmail.com>
2024-02-29 16:29:14 -0500

    29-02-2024 16:29:15

---
+++ src/app/_metronic/layout/components/toolbar/toolbar.component.ts
@@ -0,0 +1,55 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { LayoutService } from '../../core/layout.service';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss'],
+})
+export class ToolbarComponent implements OnInit, AfterViewInit {
+  @ViewChild('ktPageTitle', { static: true }) ktPageTitle: ElementRef;
+  pageTitleAttributes: {
+    [attrName: string]: string | boolean;
+  };
+  toolbarContainerCssClasses: string = '';
+  pageTitleCssClasses: string = '';
+
+  constructor(private layout: LayoutService) {}
+
+  ngOnInit(): void {
+    this.headerLeft = this.layout.getProp('header.left') as string;
+
+    this.toolbarContainerCssClasses =
+      this.layout.getStringCSSClasses('toolbarContainer');
+    this.pageTitleCssClasses = this.layout.getStringCSSClasses('pageTitle');
+    this.pageTitleAttributes = this.layout.getHTMLAttributes('pageTitle');
+  }
+
+  ngAfterViewInit() {
+    if (this.ktPageTitle) {
+      for (const key in this.pageTitleAttributes) {
+        if (
+          this.pageTitleAttributes.hasOwnProperty(key) &&
+          this.ktPageTitle.nativeElement
+        ) {
+          this.ktPageTitle.nativeElement.attributes[key] =
+            this.pageTitleAttributes[key];
+        }
+      }
+    }
+  }
+
+
+  toolbarButtonMarginClass = 'ms-1 ms-lg-3';
+  toolbarButtonHeightClass = 'w-30px h-30px w-md-40px h-md-40px';
+  toolbarUserAvatarHeightClass = 'symbol-30px symbol-md-40px';
+  toolbarButtonIconSizeClass = 'svg-icon-1';
+  headerLeft: string = 'menu';
+
+}
